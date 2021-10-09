c8ba2d66e0c13ce88eeca5b4a24cafc28f1987c2
octadelsueldo <octadelsueldo@gmail.com>
2021-10-09 19:49:36 +0200

    Agrego mi primer Hello World y comandos de declaracion de variables estaticas y dinamicas

---
+++ HelloWorld/src/helloWorld/Declaraciones_Operadores.java
@@ -0,0 +1,23 @@
+package helloWorld;
+
+public class Declaraciones_Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		
+		int b ;
+		b=7;
+		
+		// declaramos c
+		int c=b+a;
+		
+		// incremento en seis unidades para c
+		c+=6;
+		
+		System.out.println(c);
+
+	}
+
+}
+
---
+++ HelloWorld/src/helloWorld/Variables.java
@@ -0,0 +1,18 @@
+package helloWorld;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		// TODO creamos una variable. Reservamos un espacio en la memoria
+		byte edad=35;
+		
+		// TODO iniciamos la variable
+		edad=75;
+		
+		// imprimimos los resultados
+		System.out.println(edad);
+
+	}
+
+}
